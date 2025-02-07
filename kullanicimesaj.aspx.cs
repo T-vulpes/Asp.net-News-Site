ce4707da079af2d96c91d14090d96bb1c8b1d29a
Deniz <167799743+T-vulpes@users.noreply.github.com>
2025-02-07 15:34:09 +0300

    kullanicimesaj.aspx.cs

--- kullanicimesaj.aspx.cs
+++ kullanicimesaj.aspx.cs
@@ -40,7 +40,7 @@ public partial class kullanici : System.Web.UI.Page
         }
         catch (Exception ex)
         {
-            ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('Veri Görünütüleme Hatasý. Tekrar Deneyiniz!');", true);
+            ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('Veri GÃ¶rÃ¼nÃ¼tÃ¼leme HatasÄ±. Tekrar Deneyiniz!');", true);
         }
         finally
         {
@@ -59,67 +59,44 @@ public partial class kullanici : System.Web.UI.Page
 
     protected void GridView1_SelectedIndexChanged1(object sender, EventArgs e)
     {
-        // Seçilen satýrýn indeksini al
         int selectedIndex = GridView1.SelectedIndex;
-
-        // Seçilen satýrýn ID'sini al
         string secilenID = GridView1.DataKeys[selectedIndex]["Kimlik"].ToString();
-
         TextBox1.Text = secilenID;
 
-        // Silme iþlemi için kullanýcýya onay mesajý göster
-        ClientScript.RegisterStartupScript(this.GetType(), "confirm", "confirm('Bu mesajý silmek istediðinizden emin misiniz?')", true);
+        ClientScript.RegisterStartupScript(this.GetType(), "confirm", "confirm('Bu mesajÄ± silmek istediÄŸinizden emin misiniz?')", true);
     }
 
     protected void btnSil_Click(object sender, EventArgs e)
     {
-        // GridView1'de seçili bir satýr var mý kontrol edelim
         if (GridView1.SelectedRow != null)
         {
-            // Seçili satýrýn indeksini al
             int selectedIndex = GridView1.SelectedIndex;
-
-            // Seçilen satýrýn ID'sini alalým
             string secilenID = GridView1.DataKeys[selectedIndex]["Kimlik"].ToString();
 
-            // Silme iþlemini gerçekleþtirelim
             Sil(secilenID);
         }
         else
         {
-            // Eðer seçili bir satýr yoksa, kullanýcýya bir hata mesajý gösterin veya iþlemi iptal edin.
-            // Örneðin:
-            ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('Lütfen bir satýr seçin.');", true);
+            ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('LÃ¼tfen bir satÄ±r seÃ§in.');", true);
         }
     }
 
-
-
     private void Sil(string id)
     {
-        // Baðlantýyý aç
         VeriTabaniniBagla();
-
-        // Silme sorgusunu tanýmla ve baðlantýya baðlý bir SqlCommand nesnesi oluþtur
         string deleteQuery = "DELETE FROM mesajlar WHERE Kimlik = @ID";
         SqlCommand deleteCommand = new SqlCommand(deleteQuery, baglanti);
         deleteCommand.Parameters.AddWithValue("@ID", id);
 
-        // Komutu çalýþtýr ve etkilenen satýr sayýsýný geri döndür
         int affectedRows = deleteCommand.ExecuteNonQuery();
-
-        // Baðlantýyý kapat
         baglanti.Close();
 
-        // Eðer etkilenen satýr sayýsý 0 ise, silme iþlemi baþarýsýz olmuþ demektir
         if (affectedRows == 0)
         {
-            // Ýþlem baþarýsýz uyarýsý ver veya iþlemle ilgili baþka bir iþlem yap
-            ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('Silme iþlemi baþarýsýz oldu.');", true);
+            ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('Silme iÅŸlemi baÅŸarÄ±sÄ±z oldu.');", true);
         }
         else
         {
-            // Silme iþlemi baþarýlý olduðunda tekrar verileri getir
             VerileriGetir();
         }
     }
